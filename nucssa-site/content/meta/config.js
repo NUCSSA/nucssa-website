f02dd2d8ad37ca0d3cf8bf819d6fcc3b4985e731
leyiqiang <leyiqiang@gmail.com>
2018-10-05 15:55:40 -0400

    site constructing

---
+++ nucssa-site/content/meta/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: "NUCSSA", // <title>
+  shortSiteTitle: "NUCSSA", // <title> ending for posts and pages
+  siteDescription: "NUCSSA, welcome.",
+  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "NUCSSA",
+  authorTwitterAccount: "greglobinski",
+  // info
+  headerTitle: "greg lobinski",
+  headerSubTitle: "presents another one GatsbyJS starter",
+  // manifest.json
+  manifestName: "HeroBlog - a GatsbyJS starter",
+  manifestShortName: "HeroBlog", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
