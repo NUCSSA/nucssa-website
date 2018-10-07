d02f89388d84c77c8b4b1fde4cadd96e32e769ad
leyiqiang <leyiqiang@gmail.com>
2018-10-07 08:39:43 -0400

    move folder ufolder up a level

---
+++ content/meta/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: "NUCSSA", // <title>
+  shortSiteTitle: "NUCSSA", // <title> ending for posts and pages
+  siteDescription: "NUCSSA, welcome.",
+  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "NUCSSA",
+  authorTwitterAccount: "greglobinski",
+  // info
+  headerTitle: "NUCSSA",
+  headerSubTitle: "美国东北大学中国学生会",
+  // manifest.json
+  manifestName: "HeroBlog - a GatsbyJS starter",
+  manifestShortName: "HeroBlog", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
---
+++ public/sw.js
@@ -0,0 +1,103 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.2/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.2"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-2d29d06547822791bf76.js"
+  },
+  {
+    "url": "app.490624f58c95b19ca37b.css"
+  },
+  {
+    "url": "app-07e5e0eb5471f484a0a2.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-c303c650ba270fe7f9a7.js"
+  },
+  {
+    "url": "index.html",
+    "revision": "097a6dcc10b773f17ec7b04a2241c96e"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "a7009dcdfe37de3100daba7fe9ead34c"
+  },
+  {
+    "url": "component---src-pages-index-js-cca5208530eb74900790.js"
+  },
+  {
+    "url": "9-170cdd47ac2536a0dbe5.js"
+  },
+  {
+    "url": "static/d/737/path---index-6a9-olXkJz7L5uqfiT0XDnrhRJFvn1E.json",
+    "revision": "a1226616ffc78f347bc888ccc4795ac8"
+  },
+  {
+    "url": "component---src-pages-404-js-85c77ddc8e49244660c2.js"
+  },
+  {
+    "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "d0c259381c8aaa104ce23ea515d4cf8c"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/offline-plugin-app-shell-fallback/index.html", {
+  whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
+  blacklist: [/\?(.+&)?no-cache=1$/],
+});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https:/, workbox.strategies.networkFirst(), 'GET');
+"use strict";
+
+/* global workbox */
+self.addEventListener("message", function (event) {
+  var api = event.data.api;
+
+  if (api === "gatsby-runtime-cache") {
+    var resources = event.data.resources;
+    var cacheName = workbox.core.cacheNames.runtime;
+    event.waitUntil(caches.open(cacheName).then(function (cache) {
+      return Promise.all(resources.map(function (resource) {
+        return cache.add(resource).catch(function (e) {
+          // ignore TypeErrors - these are usually due to
+          // external resources which don't allow CORS
+          if (!(e instanceof TypeError)) throw e;
+        });
+      }));
+    }));
+  }
+});
